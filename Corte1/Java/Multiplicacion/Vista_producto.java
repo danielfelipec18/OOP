be2419b8fdb29096dbac38a2de96cc4caf108aab
danielfelipec18 <danielfelipec188@gmail.com>
2026-02-24 20:50:28 -0500

    Add files via upload

---
+++ Corte1/Java/Multiplicacion/Vista_producto.java
@@ -0,0 +1,17 @@
+public class Vista {
+
+    public Vista() {
+    }
+
+    public void mostrartitulo() {
+        System.out.println("Programa multiplicar dos numeros enteros");
+    }
+
+    public void mostrarnumero() {
+        System.out.println("Digite numero");
+    }
+
+    public void mostrarsalida(Numero producto) {
+        System.out.println(producto.getNumero());
+    }
+}
