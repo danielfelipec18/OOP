27ef2eaf6fec10e9d37dde084ad7551a99a45816
danielfelipec18 <danielfelipec188@gmail.com>
2026-03-26 19:46:12 -0500

    Create vistam.java

---
+++ Corte1Julio/Java/Multiplicacion/vistam.java
@@ -0,0 +1,17 @@
+public class Vistam {
+
+    public Vistam() {
+    }
+
+    public void mostrartitulo() {
+        System.out.println("Programa multiplicar dos numeros enteros");
+    }
+
+    public void mostrarnumero() {
+        System.out.println("Digite numero");
+    }
+
+    public void mostrarsalida(numerom producto) {
+        System.out.println(producto.getNumero());
+    }
+}
