e9bce0f238e37bd544fe58f26ddc45fbc81a1cb8
danielfelipec18 <danielfelipec188@gmail.com>
2026-03-26 19:47:51 -0500

    Create vistar.java

---
+++ Corte1Julio/Java/Resta/vistar.java
@@ -0,0 +1,17 @@
+public class Vistar {
+
+    public Vistar() {
+    }
+
+    public void mostrartitulo() {
+        System.out.println("Programa restar dos numeros enteros");
+    }
+
+    public void mostrarnumero() {
+        System.out.println("Digite numero");
+    }
+
+    public void mostrarsalida(Numero resta) {
+        System.out.println(resta.getNumero());
+    }
+}
