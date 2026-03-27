1fac3e5c27b4cd029d211cb023d6bfa4c42330cf
danielfelipec18 <danielfelipec188@gmail.com>
2026-03-26 19:46:38 -0500

    Create numerom.java

---
+++ Corte1Julio/Java/Multiplicacion/numerom.java
@@ -0,0 +1,25 @@
+public class numerom {
+    // Atributo o campos
+    private int numero;
+
+    // Constructores
+
+    public numerom() {
+
+    }
+
+    public numerom(int numero) {
+        this.numero = numero;
+    }
+
+    // Metodos
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+}
