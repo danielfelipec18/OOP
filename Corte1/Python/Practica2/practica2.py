b63e75b454fbe3e81a6c916b30e164d06821b1ba
danielfelipec18 <danielfelipec188@gmail.com>
2026-03-26 19:26:04 -0500

    Create practica2.py

---
+++ Corte1/Python/Practica2/practica2.py
@@ -0,0 +1,20 @@
+class Person:
+    
+    def __init__(self):
+        self.name = ""
+        self.age = 0
+   
+    def say_hello(self):
+        print(f"Hi, I'm {self.name} and I'm {self.age} years old.")
+
+def main():
+    
+    p1 = Person()
+   
+    p1.name = "Daniel"
+    p1.age = 18
+
+    p1.say_hello()
+
+if __name__ == "__main__":
+     main()
