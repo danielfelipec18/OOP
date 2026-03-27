f28804bd4683f91d110385dddae38e1be3ab248e
danielfelipec18 <danielfelipec188@gmail.com>
2026-03-26 19:48:10 -0500

    Create numeror.java

---
+++ Corte1Julio/Java/Resta/numeror.java
@@ -0,0 +1,25 @@
+public class Numeror {
+    // Atributo o campos
+    private int numero;
+
+    // Constructores
+
+    public Numeror() {
+
+    }
+
+    public Numeror(int numero) {
+        this.numero = numero;
+    }
+
+    // Metodos
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+}
